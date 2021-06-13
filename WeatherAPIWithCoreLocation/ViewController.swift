1bf50e98bcfb8dec55b1220980a00ba39c9cecb0
Barış Can Akkaya <>
2021-06-13 20:39:12 +0300

    Created project

---
+++ WeatherAPIWithCoreLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherAPIWithCoreLocation
+//
+//  Created by Barış Can Akkaya on 13.06.2021.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
